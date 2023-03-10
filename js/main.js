fb55e4ec90dbc16271cd6835c352ca7d105e5895
qvchoq <xakepq@gmail.com>
2023-03-10 23:23:36 +0300

    randomInt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const stringIsLess = function (stringInput, maxLength) {
+  return stringInput.lenght <= maxLength;
+};
