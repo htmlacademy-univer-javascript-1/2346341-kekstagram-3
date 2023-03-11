33cfd89ee2227d6fa0c3b3b21812f27734f4d3cb
qvchoq <xakepq@gmail.com>
2023-03-11 11:18:26 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-const getRandomInteger = (a, b) => {
+function getRandomInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
-
-const stringIsLess = function (stringInput, maxLength) {
+}
+// eslint-disable-next-line no-unused-vars
+function stringIsLess (stringInput, maxLength) {
   return stringInput.lenght <= maxLength;
-};
+}
+
+const description = ['хорошо', 'плохо'];
+
+function getUser(id, url) {
+  return {
+    id: id,
+    url: `photos/${url}.jpg`,
+    description: description[getRandomInteger(0, 1)],
+    likes: getRandomInteger(15, 201),
+    comments: getRandomInteger(0, 201)
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function getUsers(){
+  const arrayUsers = [];
+  for (let i = 1; i <= 25; i++) {
+    arrayUsers[i] = getUser(i, i);
+  }
+  return arrayUsers;
+}
