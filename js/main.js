be4dd24b66a4a80b81b29a4f491cec64c5808875
a <a@s.rs>
2023-04-05 13:10:42 +0300

    Done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
-function getRandomInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-// eslint-disable-next-line no-unused-vars
-function stringIsLess (stringInput, maxLength) {
-  return stringInput.lenght <= maxLength;
-}
+import {generatePhotos, stringIsLess} from './utils.js';
 
-const description = ['хорошо', 'плохо'];
 
-function getUser(id, url) {
-  return {
-    id: id,
-    url: `photos/${url}.jpg`,
-    description: description[getRandomInteger(0, 1)],
-    likes: getRandomInteger(15, 201),
-    comments: getRandomInteger(0, 201)
-  };
-}
-
-// eslint-disable-next-line no-unused-vars
-function getUsers(){
-  const arrayUsers = [];
-  for (let i = 1; i <= 25; i++) {
-    arrayUsers[i] = getUser(i, i);
-  }
-  return arrayUsers;
-}
+// eslint-disable-next-line no-console
+console.log(generatePhotos());
+// eslint-disable-next-line no-console
+console.log((stringIsLess('123', 4)));
